8c8238c90aa76eb1cb1a25e5e3abbdea8a7f0632
Leandro Menezes <lelemm@gmail.com>
2024-12-30 09:13:12 -0300

    rose pine theme

---
+++ src/manifest.ts
@@ -0,0 +1,13 @@
+import { ActualPluginManifest } from 'plugins-shared';
+
+const manifest: ActualPluginManifest = {
+  url: "https://github.com/actual-plugins/rose-pine-theme/",
+  name: "Rose Pine Theme",
+  version: "0.0.1",
+  description: "Rose Pine theme for Actual Budget.",
+  pluginType: 'client',
+  minimumActualVersion: 'v24.12.0',
+  author: "Leandro Menezes"
+};
+
+export default manifest;
---
+++ src/theming/rosepine.ts
@@ -0,0 +1,238 @@
+import { ThemeDefinition } from "plugins-shared";
+
+export const rosePineTheme: ThemeDefinition = {
+  // Page
+  pageBackground: '#191724', // Base background
+  pageBackgroundModalActive: '#1f1d2e', // Surface
+  pageBackgroundTopLeft: '#191724',
+  pageBackgroundBottomRight: '#26233a',
+  pageBackgroundLineTop: '#eb6f92', // Rose
+  pageBackgroundLineMid: '#9ccfd8', // Foam
+  pageBackgroundLineBottom: '#c4a7e7', // Iris
+  pageText: '#e0def4',
+  pageTextLight: '#908caa', // Subtle
+  pageTextSubdued: '#6e6a86', // Muted
+  pageTextDark: '#f6c177', // Gold
+  pageTextPositive: '#31748f', // Pine
+  pageTextLink: '#ebbcba', // Love
+  pageTextLinkLight: '#9ccfd8', // Foam
+
+  // Card
+  cardBackground: '#1f1d2e',
+  cardBorder: '#c4a7e7',
+  cardShadow: 'rgba(0, 0, 0, 0.5)',
+
+  // Table
+  tableBackground: '#1f1d2e',
+  tableRowBackgroundHover: '#26233a',
+  tableText: '#e0def4',
+  tableTextLight: '#908caa',
+  tableTextSubdued: '#6e6a86',
+  tableTextSelected: '#c4a7e7',
+  tableTextHover: '#f6c177',
+  tableTextInactive: '#6e6a86',
+  tableHeaderText: '#9ccfd8',
+  tableHeaderBackground: '#191724',
+  tableBorder: '#6e6a86',
+  tableBorderSelected: '#c4a7e7',
+  tableBorderHover: '#f6c177',
+  tableBorderSeparator: '#6e6a86',
+  tableRowBackgroundHighlight: '#31748f',
+  tableRowBackgroundHighlightText: '#e0def4',
+  tableRowHeaderBackground: '#26233a',
+  tableRowHeaderText: '#f6c177',
+
+  // Sidebar
+  sidebarBackground: '#191724',
+  sidebarItemBackgroundPending: '#f6c177',
+  sidebarItemBackgroundPositive: '#31748f',
+  sidebarItemBackgroundFailed: '#eb6f92',
+  sidebarItemAccentSelected: '#c4a7e7',
+  sidebarItemBackgroundHover: '#26233a',
+  sidebarItemText: '#e0def4',
+  sidebarItemTextSelected: '#ebbcba',
+
+  // Menu
+  menuBackground: '#26233a',
+  menuItemBackground: '#1f1d2e',
+  menuItemBackgroundHover: '#31748f',
+  menuItemText: '#e0def4',
+  menuItemTextHover: '#f6c177',
+  menuItemTextSelected: '#c4a7e7',
+  menuItemTextHeader: '#9ccfd8',
+  menuBorder: '#6e6a86',
+  menuBorderHover: '#c4a7e7',
+  menuKeybindingText: '#9ccfd8',
+  menuAutoCompleteBackground: '#1f1d2e',
+  menuAutoCompleteBackgroundHover: '#26233a',
+  menuAutoCompleteText: '#e0def4',
+  menuAutoCompleteTextHover: '#c4a7e7',
+  menuAutoCompleteTextHeader: '#f6c177',
+  menuAutoCompleteItemTextHover: '#f6c177',
+  menuAutoCompleteItemText: '#e0def4',
+
+  // Modal
+  modalBackground: '#1f1d2e',
+  modalBorder: '#c4a7e7',
+
+  // Mobile
+  mobileHeaderBackground: '#26233a',
+  mobileHeaderText: '#f6c177',
+  mobileHeaderTextSubdued: '#6e6a86',
+  mobileHeaderTextHover: 'rgba(246, 193, 119, 0.2)',
+  mobilePageBackground: '#191724',
+  mobileNavBackground: '#1f1d2e',
+  mobileNavItem: '#6e6a86',
+  mobileNavItemSelected: '#c4a7e7',
+  mobileAccountShadow: 'rgba(0, 0, 0, 0.5)',
+  mobileAccountText: '#9ccfd8',
+  mobileTransactionSelected: '#ebbcba',
+
+  // Mobile Themes
+  mobileViewTheme: '#26233a',
+  mobileConfigServerViewTheme: '#c4a7e7',
+
+  // Markdown
+  markdownNormal: '#ebbcba',
+  markdownDark: '#eb6f92',
+  markdownLight: '#c4a7e7',
+
+  // Buttons
+  buttonMenuText: '#e0def4',
+  buttonMenuTextHover: '#f6c177',
+  buttonMenuBackground: 'transparent',
+  buttonMenuBackgroundHover: 'rgba(246, 193, 119, 0.2)',
+  buttonMenuBorder: '#9ccfd8',
+  buttonMenuSelectedText: '#191724',
+  buttonMenuSelectedTextHover: '#191724',
+  buttonMenuSelectedBackground: '#f6c177',
+  buttonMenuSelectedBackgroundHover: '#ebbcba',
+  buttonMenuSelectedBorder: '#f6c177',
+
+  buttonPrimaryText: '#191724',
+  buttonPrimaryTextHover: '#191724',
+  buttonPrimaryBackground: '#f6c177',
+  buttonPrimaryBackgroundHover: '#ebbcba',
+  buttonPrimaryBorder: '#f6c177',
+  buttonPrimaryShadow: 'rgba(0, 0, 0, 0.5)',
+  buttonPrimaryDisabledText: '#6e6a86',
+  buttonPrimaryDisabledBackground: '#26233a',
+  buttonPrimaryDisabledBorder: '#6e6a86',
+
+  buttonNormalText: '#e0def4',
+  buttonNormalTextHover: '#f6c177',
+  buttonNormalBackground: '#26233a',
+  buttonNormalBackgroundHover: '#31748f',
+  buttonNormalBorder: '#9ccfd8',
+  buttonNormalShadow: 'rgba(0, 0, 0, 0.3)',
+  buttonNormalSelectedText: '#191724',
+  buttonNormalSelectedBackground: '#c4a7e7',
+  buttonNormalDisabledText: '#6e6a86',
+  buttonNormalDisabledBackground: '#1f1d2e',
+  buttonNormalDisabledBorder: '#6e6a86',
+
+  buttonBareText: '#e0def4',
+  buttonBareTextHover: '#f6c177',
+  buttonBareBackground: 'transparent',
+  buttonBareBackgroundHover: 'rgba(246, 193, 119, 0.2)',
+  buttonBareBackgroundActive: 'rgba(246, 193, 119, 0.3)',
+  buttonBareDisabledText: '#6e6a86',
+  buttonBareDisabledBackground: 'transparent',
+
+  // Calendar
+  calendarText: '#e0def4',
+  calendarBackground: '#191724',
+  calendarItemText: '#e0def4',
+  calendarItemBackground: '#1f1d2e',
+  calendarSelectedBackground: '#c4a7e7',
+
+  // Notices
+  noticeBackground: '#f6c177',
+  noticeBackgroundLight: '#f4a261',
+  noticeBackgroundDark: '#ebbcba',
+  noticeText: '#191724',
+  noticeTextLight: '#6e6a86',
+  noticeTextDark: '#9ccfd8',
+  noticeTextMenu: '#eb6f92',
+  noticeTextMenuHover: '#f6c177',
+  noticeBorder: '#c4a7e7',
+  warningBackground: '#ebbcba',
+  warningText: '#191724',
+  warningTextLight: '#f6c177',
+  warningTextDark: '#c4a7e7',
+  warningBorder: '#eb6f92',
+  errorBackground: '#eb6f92',
+  errorText: '#191724',
+  errorTextDark: '#c4a7e7',
+  errorTextDarker: '#c4a7e7',
+  errorTextMenu: '#f6c177',
+  errorBorder: '#ebbcba',
+  upcomingBackground: '#c4a7e7',
+  upcomingText: '#191724',
+  upcomingBorder: '#9ccfd8',
+
+  // Form
+  formLabelText: '#9ccfd8',
+  formLabelBackground: '#26233a',
+  formInputBackground: '#1f1d2e',
+  formInputBackgroundSelected: '#26233a',
+  formInputBackgroundSelection: '#c4a7e7',
+  formInputBorder: '#9ccfd8',
+  formInputTextReadOnlySelection: '#6e6a86',
+  formInputBorderSelected: '#f6c177',
+  formInputText: '#e0def4',
+  formInputTextSelected: '#ebbcba',
+  formInputTextPlaceholder: '#6e6a86',
+  formInputTextPlaceholderSelected: '#9ccfd8',
+  formInputTextSelection: '#c4a7e7',
+  formInputShadowSelected: 'rgba(189, 147, 249, 0.5)',
+  formInputTextHighlight: '#f6c177',
+
+  // Checkbox
+  checkboxText: '#e0def4',
+  checkboxBackgroundSelected: '#9ccfd8',
+  checkboxBorderSelected: '#c4a7e7',
+  checkboxShadowSelected: 'rgba(0, 0, 0, 0.4)',
+  checkboxToggleBackground: '#6e6a86',
+  checkboxToggleBackgroundSelected: '#c4a7e7',
+  checkboxToggleDisabled: '#26233a',
+
+  // Pill
+  pillBackground: '#26233a',
+  pillBackgroundLight: '#1f1d2e',
+  pillText: '#e0def4',
+  pillTextHighlighted: '#f6c177',
+  pillBorder: '#9ccfd8',
+  pillBorderDark: '#6e6a86',
+  pillBackgroundSelected: '#c4a7e7',
+  pillTextSelected: '#191724',
+  pillBorderSelected: '#f6c177',
+  pillTextSubdued: '#6e6a86',
+
+  // Reports
+  reportsRed: '#eb6f92',
+  reportsBlue: '#9ccfd8',
+  reportsGreen: '#31748f',
+  reportsGray: '#6e6a86',
+  reportsLabel: '#e0def4',
+  reportsInnerLabel: '#908caa',
+
+  // Misc
+  noteTagBackground: '#26233a',
+  noteTagBackgroundHover: '#1f1d2e',
+  noteTagText: '#f6c177',
+
+  floatingActionBarBackground: '#26233a',
+  floatingActionBarBorder: '#c4a7e7',
+  floatingActionBarText: '#e0def4',
+
+  tooltipText: '#e0def4',
+  tooltipBackground: '#1f1d2e',
+  tooltipBorder: '#c4a7e7',
+
+  // Budget
+  budgetOtherMonth: '#1f1d2e',
+  budgetCurrentMonth: '#26233a',
+  budgetHeaderOtherMonth: '#6e6a86',
+  budgetHeaderCurrentMonth: '#c4a7e7',
+};
